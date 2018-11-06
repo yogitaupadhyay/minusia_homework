cfaa4ca3f22de4627e0a8a1c29872cadd97d20ea
yogitaupadhyay <yogitau2008@gmail.com>
2018-11-06 18:40:14 +0530

    first commit

---
+++ christmas.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>minushia</title>
+         <link rel="stylesheet" href="style.css">
+         <link rel="shortcut icon "href="TAB ICON BROWSER-01.png">        <!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<meta name="veiwport" content="width=device-width initial scale=1">
+    </head>
+    <body>
+        <div id="vd"> <video width="100%" height="80%" style="border: 20% solid black;border-bottom: 40% solid black" controls >
+                <source src="Cute Christmas Cartoon(funnymp4.net).mp4" type="video/mp4">
+  
+  
+           </video></div>
+    </body></html>
+
---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>minushia</title>
+         <link rel="stylesheet" href="style.css">
+         <link rel="shortcut icon "href="TAB ICON BROWSER-01.png">        <!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<meta name="veiwport" content="width=device-width initial scale=1">
+    </head>
+    <body>
+        
+        <div id="back">
+            <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button btn-primary" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar1">
+        <span class="icon-bar" ></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar1">
+      <ul class="nav navbar-nav navbar-right" id="pri">
+       
+          <li><a href="#" ><span class="glyphicon glyphicon-home" style="height: 25px"></span></a></li>
+          <li><a href="#">ABOUT US</a></li> 
+          <li class="active"><a href="index.php">CLIENTS</a></li> 
+        <li><a href="#">SEVICES</a></li> 
+        <li><a href="#">FREEBIES</a></li> 
+        <li><a href="#">CONTACTS</a></li> 
+       
+      </ul>
+    </div>
+  </div>
+</nav> 
+             <img src="TAB ICON BROWSER-01.png" style="width:12%;height:12%">
+            
+        
+        </div>
+        <div class="container-fluid">
+       <div id="hero-image">
+           <div id="text">
+               <h1>CLIENTS</h1>
+           </div>
+ 
+       </div></div>
+        <div  id="lowerd">
+            
+                <h1 style="color:#42dff4;text-align: center"><ul class="list-inline" id="lowerdul">
+                   <li id="m"><a href="#">CLIENTS</a></li>
+  <li><a href="#">.PARTNERS</a></li>
+  <li><a href="#">.COLLABORATION</a></li>
+  
+                    </ul></h1>
+            <div class="container-fluid" style="margin:5%">
+                <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-2"><div id="first"><a href="cute.php"><img src="i7.jpg"   class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a></div><br><br></div>
+                <div class="col-xs-2"><a href="tigrer.php"><img src="i8.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                <div class="col-xs-2"><a href="squirrel.php"><img src="i9.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                <div class="col-xs-2"><a href="dolphin.php"><img src="i10.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                <div class="col-xs-2" ><a href="squirrel.php"><img src="i9.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                <div class="col-xs-2"><a href="dolphin.php"><img src="i10.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+            </div>
+        </div>  
+                <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-2"><a href="clownfish.php"><img src="i3.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                        <div class="col-xs-2"><a href="BANNER.php"><img src="i4.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                        <div class="col-xs-2"><a href="GATOS.php"><img src="i5.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                        <div class="col-xs-2"><a href="christmas.php"><img src="i6.png" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                <div class="col-xs-2"><a href="GATOS.php"><img src="i5.jpg" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                        <div class="col-xs-2"><a href="christmas.php"><img src="i6.png" class="img-responsive img-thumbnail" style='width: 40%;height: 40%'></a><br><br></div>
+                
+            </div>
+        </div>  
+            </div>      
+            <h1 style="color:#42dff4;text-align: center">subscribe</h1>
+            <h4 style="color:white;text-align: center">enter your email to subscribe and get your freebie</h4>
+            <p style="text-align: center"><input type="email" placeholder="email">      <button type="btn btn-danger">subscribe</button></p>
+            <br>
+            <br>
+            <div id="contact" style="margin-left: 10%;color:white; "><h3>ET IN TOUCH</h3>
+                <address>
+                    <h4><a href="tel:7697526603">+917697526603</a> </h4>
+            <h4><a href="tel:7697526603">+917697526603</a> </h4>
+            <h4><a href="tel:7697526603">+917697526603</a> </h4>
+            <h4><a href="info@cassieandku.com">info@cassieandku.com</a></h4> 
+                </address>
+           
+            </div>
+            <br>
+            
+            
+            <h5  style="margin: 5%;color: white"> @<span id="demo"></span>.All  Rights Reserved.  </h5>
+            <h5 style="text-align: right"> <a href="https://www.facebook.com/yogita.upadhyay.161" style="color: white">
+                    <img  src="facebook-Icon-Grijs.png" style="width:2%;height: 2%"> find us at facebook</a>
+                    <a href="#" style="color: white">
+                        <img  src="Twitter-Icon-Grijs.png" style="width:2%;height: 2%"> find us at twitter</a>
+                        <a href="#"  style="color: white">
+                            <img  src="images.png" style="width:2%;height: 2%"> find us at insta</a>
+                        </h5>
+            <script>
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getFullYear();
+</script>
+        </div>
+    </body>
+</html>
